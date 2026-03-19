3dba2c9f51e7315aeed1d3418852cf2e7cd54cd5
21JBH <sbassa0504@gmail.com>
2026-03-18 20:13:36 -0500

    primer commit

---
+++ src/main/java/com/spring/app/social_action_network/SocialActionNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.spring.app.social_action_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialActionNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialActionNetworkApplication.class, args);
+	}
+
+}
